9a90cd76a9d145a57511bc6e92cc9b1bc17c3e09
Aliaksey Shapel <aliaksey.shapel@gmail.com>
2020-07-07 17:05:36 +0300

    Updates

---
+++ precache-manifest.4fd3cb802464c6e6e37e3f4bc2b26394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc570c002224a8e4de779a3a74b2cc96",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5410c9f9e24c443b6085",
+    "url": "/monsters-rolodex/static/css/main.4465b924.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5410c9f9e24c443b6085",
+    "url": "/monsters-rolodex/static/js/main.ab42ad0c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
